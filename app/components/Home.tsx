c6b526c226e22b182788f5e805aa3d9ae224b371
Your name <Your email>
2024-12-16 16:09:54 +0100

    Modif background

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -35,9 +35,10 @@ const styles: { [key: string]: React.CSSProperties } = {
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
+    /*
     backgroundImage: 'url("/background-image.jpg")', // Remplace par une image dans /public
     backgroundSize: 'cover',
-    backgroundAttachment: 'fixed',
+    backgroundAttachment: 'fixed',*/
   },
   description: {
     fontSize: '18px',
